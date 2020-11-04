70154ebdb0e0bf342ff56e5bbb0126fe60e3244e
jamesgilley <70067688+jamesgilley@users.noreply.github.com>
2020-11-04 12:54:00 -0800

    starting the code

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+console.log("loaded")
+function myFunction(event) {
+event.preventDefault();
+userInput = document.getElementById('cityName').value;
+//console.log(e)
+console.log(userInput);
+alert(userInput)
+}
+
