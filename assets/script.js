a29e642c2d68f3e3bb875b7120b41c91e11bebee
jamesgilley <70067688+jamesgilley@users.noreply.github.com>
2020-11-05 14:20:30 -0800

    update html

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,25 @@
+var buttonSub = document.getElementById("submit")
+var cityDiv = document.getElementById("cityStor")
+cityStor()
 console.log("loaded")
 function myFunction(event) {
 event.preventDefault();
 userInput = document.getElementById('cityName').value;
 //console.log(e)
 console.log(userInput);
-alert(userInput)
+localStorage.setItem("city", userInput)
+cityStor()
 }
+
+
+function cityStor(){
+var previosCity = localStorage.getItem("city")
+//push previos city into an array define globally, append arrey,use jsonparse and json strinafy so you can take out and loop through
+//unit 4 activity 28 
+cityDiv.append(previosCity)
+
+}
+
+//unit 4 web apis activity 11
+buttonSub.addEventListener("click", myFunction)
 
